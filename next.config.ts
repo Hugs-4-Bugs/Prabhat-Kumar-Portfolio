f6382bab078632fb277667da299c7c03fd89deb5
Prabhat Kumar <prabhatkumarssm72@gmail.com>
2025-10-27 11:15:22 +0000

    photo is not being displayed

--- next.config.ts
+++ next.config.ts
@@ -33,6 +33,12 @@ const nextConfig: NextConfig = {
         hostname: 'storage.googleapis.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'monorepo-prod.appspot.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
