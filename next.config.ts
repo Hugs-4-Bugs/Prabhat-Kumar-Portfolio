abd3d4c6fc368fdb8289dbfadf9515c9e2a81fa3
Prabhat Kumar <prabhatkumarssm72@gmail.com>
2025-10-27 12:55:56 +0000

    my image is not existing in this project directory and because of that i

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -23,17 +24,11 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https' ,
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
       },
-      {
-        protocol: 'https',
-        hostname: 'storage.googleapis.com',
-        port: '',
-        pathname: '/**',
-      }
     ],
   },
 };
