e7345c82635b79f4fb5c1537cd20ebfab51a493e
Prabhat Kumar <prabhatkumarssm72@gmail.com>
2025-10-27 15:28:15 +0000

    Try fixing this error: `Console Error: Encountered two children with the

--- src/components/sections/projects.tsx
+++ src/components/sections/projects.tsx
@@ -61,7 +61,7 @@ export function Projects() {
         <AnimatePresence>
           {filteredProjects.slice(0, visibleCount).map((project, index) => (
             <motion.div
-              key={`${activeFilter}-${project.title}`}
+              key={`${activeFilter}-${project.name}`}
               layout
               initial={{ opacity: 0, scale: 0.8 }}
               animate={{ opacity: 1, scale: 1 }}
