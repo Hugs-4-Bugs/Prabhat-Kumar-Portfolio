916b642a5dd08d8de0d5ae2c0a47e03ab7150ed1
Hugs-4-Bugs <prabhathkumar.cse@skit.org.in>
2026-05-27 10:14:32 +0530

    Update genkit.ts

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -7,6 +7,6 @@ export const ai = genkit({
       apiKey: process.env.GEMINI_API_KEY,
     }),
   ],
-  // model: 'googleai/gemini-1.5-flash',
-  model: 'googleai/gemini-2.5-flash',
+  model: 'googleai/gemini-1.5-flash',
+  // model: 'googleai/gemini-2.5-flash',
 });
